f5d25a654c21ee22675a3c256a168e0eb52ca67d
davenportnw <davenportnw@gmail.com>
2020-09-21 20:41:55 -0500

    Connect server and database

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const connectDB = require('./db/connection');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+connectDB();
+
+const PORT = process.env.Port || 3000;
+app.listen(PORT, () => console.log("Connected to server on " + PORT));
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+
+
