3dcfb743d6ac209ab4d84e4ab83e740fe36abc62
Ty D'Angelo <tydangelo18@gmail.com>
2020-09-23 15:30:46 -0500

    Created Tweets Routes, export router, bring tweet routes into server, and tested get route with Postman.

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,2 +1,47 @@
-// Tweet Routes will be here
-let tweetroute = x;
+// Require Dependencies
+const express = require('express');
+const router = express.Router();
+// Bring in Tweet Model so the Route methods can reference
+const Tweet = require('../../models/Tweet');
+
+// Routes
+    
+    // GET Route
+    // Tested with POSTMAN
+router.get('/', (req, res) => {
+    // Fetch all items from the DB
+    Tweet.find().sort({date: -1})
+    .then(tweets => res.json(tweets))
+});
+
+    // POST Route
+    // NEED TO TEST WITH POSTMAN
+router.put('/', (req, res) => {
+    const newTweet = new Tweet({
+        // Follow the Schema
+        name: req.body.name,
+        screen_name: req.body.screen_name,
+        date: req.body.date,
+        profile_pic: req.body.profile_pic,
+        tweet_text: req.body.tweet_text,
+        reply_count: req.body.reply_count,
+        retweet_count: req.body.retweet_count,
+        favorite_count: req.body.favorite_count,
+        follower_count: req.body.follower_count,
+        following_count: req.body.following_count,
+        tweet_count: req.body.tweet_count
+    });
+    newTweet.save()
+    .then(tweet => res.json(tweet));
+});
+
+    // DELETE Route
+    // NEED TO TEST WITH POSTMAN
+router.delete('/:id', (req, res) => {
+    Tweet.findById(req.params.id).then(tweet => tweet.remove()
+    .then(() => res.json({success: true })))
+    .catch(err => res.status(404).json({success: false}))
+});
+
+module.exports = router;
+
