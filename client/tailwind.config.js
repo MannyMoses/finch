3d8c5fddfa74702a0cba712be46ffc156d8fb265
Alba Osmanaj <albaos91@gmail.com>
2020-09-30 18:41:35 -0500

    merging package-lock.json

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    purge: [
-      'src/**/*.js',
-      'src/**/*.jsx',
-      'src/**/*.ts',
-      'src/**/*.tsx',
-      'public/**/*.html',
-    ],
-    theme: {
-      extend: {},
-    },
-    variants: {},
-    plugins: [],
-  }
+  purge: [
+    "src/**/*.js",
+    "src/**/*.jsx",
+    "src/**/*.ts",
+    "src/**/*.tsx",
+    "public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  fontFamily: {
+    display: ["Helvetica Neue"],
+  },
+  variants: {},
+  plugins: [],
+};
