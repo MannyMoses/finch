bb17d14384f89786c9f43bae1637b488bdeb7b1f
Alba Osmanaj <albaos91@gmail.com>
2020-09-30 21:00:25 -0500

    added the correct rendering on index.js, checked with the team

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser, logoutUser } from './actions/authActions';
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
-import { Provider } from 'react-redux';
-import Register from './pages/Register/register';
-import Login from './pages/Login/login';
-import Dashboard from './pages/Dashboard/dashboard';
-import store from './store';
-import PrivateRoute from './components/private-route/PrivateRoute';
-import API from '../src/utils/API';
-
-
+import { Provider } from "react-redux";
+import Register from "./pages/Register/register";
+import Login from "./pages/Login/login";
+import Dashboard from "./pages/Dashboard/dashboard";
+import store from "./store";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import API from "../src/utils/API";
 
 //Check for token to keep user logged in
 if (localStorage.jwtToken) {
@@ -25,31 +23,30 @@ if (localStorage.jwtToken) {
   store.dispatch(setCurrentUser(decoded));
 
   //Check for expired token
-  const currentTime = Date.now() /1000;
+  const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     //Logout user
     store.dispatch(logoutUser());
 
     //Redirect to login
-    window.location.href="./";
+    window.location.href = "./";
   }
 }
 
 class App extends Component {
   render() {
-    return(
+    return (
       <Provider store={store}>
         <Router>
-          <Route exact path="/" component = {Login}/>
-          <Route exact path="/register" component={Register}/>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
           <Switch>
-            <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
           </Switch>
         </Router>
       </Provider>
-    )
+    );
   }
 }
-
 
 export default App;
