df6cb321497e653ad5dfcfe8bc978b1a88e029a4
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-03 13:30:35 -0500

    Created Dashboard components.

---
+++ client/src/components/Dashboard/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Title extends Component {
+    render() {
+        return (
+            <div className="titleDiv">
+                <h1 className="title">Finch</h1>
+                <h5 className="subTitle">Twitter Analytics Tool</h5>
+            </div>
+        )
+    }
+};
+
+export default Title;
