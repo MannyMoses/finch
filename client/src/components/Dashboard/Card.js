cd487e4b42ab02410955d923540daf5873c7f6ee
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-05 21:20:22 -0500

    Got twitter api response to be a twitter array and an user array vs one object. Mapped through both arrays to render.

--- client/src/components/Dashboard/Card.js
+++ client/src/components/Dashboard/Card.js
@@ -4,21 +4,46 @@ class Card extends Component {
     // declare repsonses as a variable
     // set state with repsonses
     constructor(props) {
-        const { tweet, date, likes, retweets, replies } = props
-        super(props)
+        const { 
+            tweet,
+            date, 
+            likes, 
+            retweets, 
+            replies, 
+            name, 
+            username, 
+            pic, 
+            following, 
+            followers, 
+            userTweet 
+                } = props
+        
+                super(props)
         this.state = {
             tweet,
             date,
             likes,
             retweets,
-            replies
+            replies, 
+            name, 
+            username, 
+            pic, 
+            following, 
+            followers, 
+            userTweet 
         }
     }
     render() {
         return (
             <div className="card">
                 <div className="tweet">
-                    <h4 className="tweet">{this.props.tweet}</h4>
+                    <h4 className="name">{this.props.name}</h4>
+                    <h4 className="userName">@{this.props.username}</h4>
+                    <img src={this.props.pic} alt=""></img>
+                    <p className="following">{this.props.following}</p>
+                    <p className="followers">{this.props.followers}</p>
+                    <p className="userTweet">{this.props.userTweet}</p>
+                    <p className="tweet">{this.props.tweet}</p>
                     <p className="date">{this.props.date}</p>
                     <p className="likes">Likes: {this.props.likes}</p>
                     <p className="retweets">Retweets: {this.props.retweets}</p>
--- client/src/utils/TwitterAPI.js
+++ client/src/utils/TwitterAPI.js
@@ -7,8 +7,12 @@ class TwitterAPI extends Component {
     constructor(props) {
         super(props)
         // Initial State
+        // Tweet Array
+        // Users Array
+        // Search Value Input String
         this.state = {
-            twitterResponse: {},
+            tweets: [],
+            users: [],
             searchVal: ''
         }
 
@@ -16,37 +20,34 @@ class TwitterAPI extends Component {
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    // Search Method
+    // Search Methods
     
     handleChange(event){
-        this.setState({searchVal: event.target.value})
+        this.setState({...this.state,searchVal: event.target.value })
     }
 
     handleSubmit(event){
         event.preventDefault();
         console.log(this.state.searchVal);
         axios.get(`/api/gettweets?searchVal=${this.state.searchVal}`).then(res => {
-            console.log(res)
+            // console.log(res)
             // Set Initial State
+            // State of Tweets Object Array
+            // State of Users Object Array
         this.setState({
-
-            twitterResponse: res.data
+            ...this.state,tweets:res.data.data,users:res.data.includes.users
         })
 
-        console.log('=========> twitRes', this.state.twitterResponse);
+        // Log the Tweet & User arrays
+        console.log('=========> twitRes', this.state.tweets, this.state.users);
     })
     }
 
-
-
     render() {
-        // Map through Tweet and User Object to render Twitter metrics to the front-end
         return (
             <div className="twitterAPI">
-            <SearchBar handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
-           
-            <List tweets={this.twitterResponse} />
-           
+                <SearchBar handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+                <List tweets={this.state.tweets} users={this.state.users} />
             </div>
         )
     }
