afc7cc51c32c8dcd159cae950ca2786c016c0ab2
Alba Osmanaj <albaos91@gmail.com>
2020-10-05 21:28:18 -0500

    worked on form.js by adding container,box and buttons using Material UI

--- client/src/components/Login/form.js
+++ client/src/components/Login/form.js
@@ -1,8 +1,31 @@
-import React, { Component } from "react";
-import "../styles/Login.css";
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
 
-class Login extends Component {
-    render() {
-      
-
-export default Login;
+export default function SimpleContainer() {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Box component="span" m={5}>
+          <Typography
+            variant="h3"
+            gutterBottom
+            component="div"
+            style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+          >
+            Finch
+          </Typography>
+          <Button variant="outlined" color="darkgray">
+            Login
+          </Button>
+          <Button variant="outlined" color="darkgray">
+            Register
+          </Button>
+        </Box>
+      </Container>
+    </React.Fragment>
+  );
+}
