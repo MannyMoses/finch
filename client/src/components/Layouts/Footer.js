5d548a33bccfa06e50ddbf2cf34caa2906665af4
Alba Osmanaj <albaos91@gmail.com>
2020-10-07 21:25:31 -0500

    played with the CSS of the login.Added background color. Still looking on how to center the container and add space on buttons

--- client/src/components/Layouts/Footer.js
+++ client/src/components/Layouts/Footer.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react";
 
-export default props =>
-    <div>
-        <h1>Footer</h1>
-    </div>
+export default (props) => (
+  <div>
+    <h1>App is brought to you by Node Cap</h1>
+  </div>
+);
--- client/src/components/Login/form.js
+++ client/src/components/Login/form.js
@@ -1,26 +1,39 @@
 import React from "react";
+import Header from "./components/Layouts/Header";
+import Footer from "./components/Layouts/Footer";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { Typography, Container, Box } from "@material-ui/core/Typography";
+import { Typography, Container, Box } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
-
+import Button from "@material-ui/core/Button";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  root: {}, // a style rule
+  label: {}, // a nested style rule
+});
 export default function SimpleContainer() {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box component="span" m={5}>
+      <Container maxWidth="md">
+        <Box
+          display="block"
+          width={750}
+          height={500}
+          bgcolor="#f44336"
+          justifyContent="center"
+        >
           <Typography
             variant="h3"
             gutterBottom
             component="div"
-            style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+            style={{ backgroundColor: "none", height: "10vh" }}
           >
             Finch
           </Typography>
-          <Button size="large" variant="outlined" color="darkgray">
+          <Button size="large" variant="outlined" color="darkgray" mb="1">
             Register
           </Button>
-
           <form className={classes.root} noValidate autoComplete="off">
             <TextField
               id="outlined-basic"
