333c409b1be4c8188431f8f438e590e24f1bcbd8
davenportnw <davenportnw@gmail.com>
2020-10-09 15:46:01 -0500

    Add basic fields

--- client/src/pages/Register/register.js
+++ client/src/pages/Register/register.js
@@ -1,9 +1,25 @@
 import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom'; 
+import {  withRouter } from 'react-router-dom'; 
 import React, { Component } from 'react';
 import { registerUser } from '../../actions/authActions';
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+
+
+
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 import './register.css';
 
@@ -19,6 +35,8 @@ class Register extends Component {
         };
     }
 
+
+      
     componentDidMount() {
         //If logged in, redirect to dashboard
         if (this.props.auth.isAuthenticated) {
@@ -37,6 +55,7 @@ class Register extends Component {
     onChange = e => {
         this.setState({ [e.target.id]: e.target.value});
     };
+    
 
     onSubmit = e => {
         e.preventDefault();
@@ -50,66 +69,109 @@ class Register extends Component {
         console.log("newUser",newUser);
         this.props.registerUser(newUser, this.props.history);
     };   
-     
+
+    
 
     render() {
         const { errors } = this.state;
+    
+      
+        return (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Avatar>
+                    <LockOutlinedIcon />
+                </Avatar>
+            <Typography component="h1" variant="h5">
+            Sign Up
+            </Typography>
+            <form noValidate>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} >
+                        <TextField
+                             onChange={this.onChange}
+                             value={this.state.name}
+                             error={errors.name}
+                             id="name"
+                             type="text"
+                             className={classnames("", {
+                               invalid: errors.name
+                             })}
+                              autoComplete="fname"
+                              name="name"
+                              variant="outlined"
+                              required
+                              fullWidth
+                              id="name"
+                              label="Name"
+                              autoFocus
+                            />
+                    </Grid>
+                    <Grid item xs={12}>
+                    <TextField
+                        onChange={this.onChange}
+                        value={this.state.email}
+                        error={errors.email}
+                        id="email"
+                        type="email"
+                        className={classnames("", {
+                        invalid: errors.email
+                        })}
+                        variant="outlined"
+                        required
+                        fullWidth
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                    />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                        onChange={this.onChange}
+                        value={this.state.password}
+                        error={errors.password}
+                        id="password"
+                        type="password"
+                        className={classnames("", {
+                        invalid: errors.password
+                        })}
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        autoComplete="current-password"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                    <TextField
+                        onChange={this.onChange}
+                        value={this.state.password2}
+                        error={errors.password2}
+                        id="password2"
+                        type="password"
+                        className={classnames("", {
+                        invalid: errors.password2
+                        })}
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="password2"
+                        label="Re-enter Password"
+                    />
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        >
+                        Sign Up
+                    </Button>
+                </Grid>
+            </form>
+            </Container>
 
-        return(
-          <form noValidate onSubmit={this.onSubmit}>
-        
-            <label htmlFor="name">Name</label>
-            <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="name"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.name
-                  })}
-                />
-              
-            <label htmlFor="email">Email</label>
-            <span className="red-text">{errors.name}</span>
-            <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email
-                  })}
-                />
-
-            <label htmlFor="password">Password</label>
-            <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password
-                  })}
-                />
-
-
-            <label htmlFor="password2">Confirm Password</label>  
-            <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password2
-                  })}
-                />
-                
-            <button type="submit">Sign Up</button>
-          </form>  
         );
     }
 }
