0e0e6519b49e2656ff757ad00e45f28cff219917
Ty D'Angelo <tydangelo18@gmail.com>
2020-10-09 18:18:56 -0500

    Render Saved Tweets to Front-End Component.

--- client/src/components/Saved/SavedTweets.js
+++ client/src/components/Saved/SavedTweets.js
@@ -5,14 +5,14 @@ class SavedTweets extends Component {
     constructor(props) {
         super(props) 
         this.state ={
-            posts: {}
+            posts: []
         };
-
-        this.componentDidMount = () => {
+    }
+        componentDidMount = () => {
             this.getTweets();
         };
 
-        this.getTweets = () => {
+        getTweets = () => {
             axios.get('/api/tweets')
             .then((response) => {
                 const data = response.data;
@@ -26,23 +26,20 @@ class SavedTweets extends Component {
             })
         }
 
-           {this.state.posts.map((data, index) => {
-                return <div key={index}>
-                    <img alt ="" className="pic">{data.pic}</img>
-                    <h2 className="name">{data.name}</h2>
-                    <p className="username">{data.username}</p>
-                    <p className="following">{data.following}</p>
-                    <p className="followers">{data.followers}</p>
-                    <p className="userTweet">{data.userTweet}</p>
-                    <p className="tweet">{data.tweet}</p>
-                    <p className="date">{data.date}</p>
-                    <p className="likes">{data.likes}</p>
-                    <p className="retweets">{data.retweets}</p>
-                    <p className="replies">{data.replies}</p>
-                </div>
-            })};
-        }
+    
+
+    render() {
+        return (
+            <div className="savedList">
+                {this.state.posts.map((post) => (
+                    <p className="name">{post.name}</p>
+                ))}
+            </div>
+        )
     }
+}
+
+    
 
 
 
