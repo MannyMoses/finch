cdfb4a510c8509f73b309294dc9b72b0bc4bc1eb
MannyMoses <msm.prof@hotmail.com>
2020-10-09 17:59:36 -0500

    consolidated imports

--- client/src/components/Dashboard/Drawer/Drawer.jsx
+++ client/src/components/Dashboard/Drawer/Drawer.jsx
@@ -1,57 +1,154 @@
-import React from "react";
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import {
-  Drawer as MUIDrawer,
-  ListItem,
+  Drawer,
+  CssBaseline,
+  AppBar,
+  Toolbar,
   List,
+  Typography,
+  Divider,
+  IconButton,
+  ListItem,
   ListItemIcon,
-  ListItemText,
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
-import { withRouter } from "react-router-dom";
+  ListItemText
+} from '@material-ui/core/Drawer';
+import {
+MenuIcon,
+InboxIcon,
+MailIcon,
+ChevronLeftIcon,
+ChevronRightIcon
+} from '@material-ui/icons';
+const drawerWidth = 240;
 
-const useStyles = makeStyles({
-  drawer: {
-    width: "190px",
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
   },
-});
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(1),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
 
-const Drawer = (props) => {
-  const { history } = props;
+export default function PersistentDrawerLeft() {
   const classes = useStyles();
-  const itemsList = [
-    {
-      text: "Home",
-      icon: <InboxIcon />,
-      onClick: () => history.push("/"),
-    },
-    {
-      text: "About",
-      icon: <MailIcon />,
-      onClick: () => history.push("/about"),
-    },
-    {
-      text: "Contact",
-      icon: <MailIcon />,
-      onClick: () => history.push("/contact"),
-    },
-  ];
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <MUIDrawer variant="permanent" className={classes.drawer}>
-      <List>
-        {itemsList.map((item, index) => {
-          const { text, icon, onClick } = item;
-          return (
-            <ListItem button key={text} onClick={onClick}>
-              {icon && <ListItemIcon>{icon}</ListItemIcon>}
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="static"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            {this.name+"'s" + ' ' + "Dashboard"}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
-          );
+          ))}
+        </List>
+        <Divider />
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
         })}
-      </List>
-    </MUIDrawer>
+      >
+        <div className={classes.drawerHeader} />
+        
+      </main>
+    </div>
   );
-};
-
-export default withRouter(Drawer);
