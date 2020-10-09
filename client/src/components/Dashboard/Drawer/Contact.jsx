307968f797e59e9a587228b4019d82ce045c9dc2
MannyMoses <msm.prof@hotmail.com>
2020-10-09 16:08:08 -0500

    added placeholder code for drawer components

---
+++ client/src/components/Dashboard/Drawer/Contact.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>Contact placeholder</div>;
+};
+
+export default About;
---
+++ client/src/components/Dashboard/Drawer/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>Home placeholder</div>;
+};
+
+export default About;
