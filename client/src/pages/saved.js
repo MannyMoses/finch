f4067928b1d526bdc91568610b8afc16cb71e1ee
Alba Osmanaj <albaos91@gmail.com>
2020-10-10 00:28:55 -0500

    indicated saved tweets on the card in case we dont get to pass the props at least we can show what goes there

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -166,11 +166,7 @@ export default function PersistentDrawerLeft() {
             <CardActionArea>
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Lizard
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  Lizards are a widespread group of squamate reptiles, with over
-                  6,000 species, ranging across all continents except Antarctica
+                  Saved Tweets
                 </Typography>
               </CardContent>
             </CardActionArea>
