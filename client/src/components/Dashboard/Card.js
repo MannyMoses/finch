5981bdd8d6e53d6ec0316c794dd5914dd7f8fc27
MannyMoses <msm.prof@hotmail.com>
2020-10-09 22:21:43 -0500

    fixed saved tweets

--- client/src/components/Dashboard/Card.js
+++ client/src/components/Dashboard/Card.js
@@ -1,81 +1,78 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import SaveButton from './SaveButton';
+import React, { Component } from "react";
+import axios from "axios";
+import SaveButton from "./SaveButton";
 
 class Card extends Component {
-    // declare repsonses as a variable
-    // set state with repsonses
-    constructor(props) {
-        const { 
-            tweet,
-            date, 
-            likes, 
-            retweets, 
-            replies, 
-            name, 
-            username, 
-            pic, 
-            following, 
-            followers, 
-            userTweet 
-                } = props
-        
-                super(props)
-        this.state = {
-            tweet,
-            date,
-            likes,
-            retweets,
-            replies, 
-            name, 
-            username, 
-            pic, 
-            following, 
-            followers, 
-            userTweet 
-        }
+  // declare responses as a variable
+  // set state with repsonses
+  constructor(props) {
+    const {
+      tweet,
+      date,
+      likes,
+      retweets,
+      replies,
+      name,
+      username,
+      pic,
+      following,
+      followers,
+      userTweet,
+    } = props;
 
-    }
+    super(props);
+    this.state = {
+      tweet,
+      date,
+      likes,
+      retweets,
+      replies,
+      name,
+      username,
+      pic,
+      following,
+      followers,
+      userTweet,
+    };
+  }
 
-    handleClick = (event) => {
-        event.preventDefault();
+  handleClick = (event) => {
+    event.preventDefault();
 
-     axios({
-         url: '/api/tweets/saved',
-         method: 'POST',
-         data: this.props
-     })
-     .then(() => {
-         console.log("Data sent to server!");
-     })
-     .catch(() => {
-         console.log('Server Error');
-     });
-    }
+    axios({
+      url: "/api/tweets/saved",
+      method: "POST",
+      data: this.props,
+    })
+      .then(() => {
+        console.log("Data sent to server!");
+      })
+      .catch(() => {
+        console.log("Server Error");
+      });
+  };
 
+  render() {
+    return (
+      <div className="card">
+        <div className="tweet">
+          <img className="pic" src={this.props.pic} alt=""></img>
+          <h2 className="name">{this.props.name}</h2>
+          <h6 className="userName">@{this.props.username}</h6>
+          <p className="following">{this.props.following} Following</p>
+          <p className="followers">{this.props.followers} Followers</p>
+          <p className="userTweet">{this.props.userTweet} Tweets</p>
+          <p className="tweet">{this.props.tweet}</p>
+          <p className="date">{this.props.date}</p>
+          <p className="likes">{this.props.likes} Likes</p>
+          <p className="retweets">{this.props.retweets} Retweets</p>
+          <p className="replies">{this.props.replies} Replies</p>
+        </div>
 
-    render() {
-        return (
-            <div className="card">
-                <div className="tweet">
-                    <img className="pic" src={this.props.pic} alt=""></img>
-                    <h2 className="name">{this.props.name}</h2>
-                    <h6 className="userName">@{this.props.username}</h6>
-                    <p className="following">{this.props.following} Following</p>
-                    <p className="followers">{this.props.followers} Followers</p>
-                    <p className="userTweet">{this.props.userTweet} Tweets</p>
-                    <p className="tweet">{this.props.tweet}</p>
-                    <p className="date">{this.props.date}</p>
-                    <p className="likes">{this.props.likes} Likes</p>
-                    <p className="retweets">{this.props.retweets} Retweets</p>
-                    <p className="replies">{this.props.replies} Replies</p>
-                </div>
-                
-                  <SaveButton handleClick={this.handleClick} />
-                
-            </div>
-        )
-    }
+        <SaveButton handleClick={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default Card;
