352fc9b5a83043dcc2da7322e8b5732ca9e9e3dc
Alba Osmanaj <albaos91@gmail.com>
2020-10-09 23:51:10 -0500

    created the card on the saved.js file.Need to figure out how to pass the props

--- client/src/components/Saved/SavedTweets.js
+++ client/src/components/Saved/SavedTweets.js
@@ -3,65 +3,24 @@ import axios from "axios";
 import DeleteButton from "../Saved/DeleteButton";
 
 class SavedTweets extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [],
-        };
-    }
-
-    componentDidMount = () => {
-        this.getTweets();
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
     };
+  }
 
+  componentDidMount = () => {
+    this.getTweets();
+  };
 
-        handleDelete = (event) => {
-            event.preventDefault();
-            axios({
-                url: '/api/tweets/:id',
-                method: "delete",
-                data: this.state.posts
-            })
-            .then(() => {
-                console.log("Data Deleted!");
-            })
-            .catch((err) => {
-                console.log("Server Error!");
-                console.log(err);
-            });
-        }
-
-
-getTweets = () => {
-  axios
-    .get("/api/tweets")
-    .then((response) => {
-      const data = response.data;
-      console.log("Data Received!");
-      this.setState({ posts: data });
-      console.log(this.state.posts);
+  handleDelete = (event) => {
+    event.preventDefault();
+    axios({
+      url: "/api/tweets/:id",
+      method: "delete",
+      data: this.state.posts,
     })
-    .catch(() => {
-      console.log("Error with Database!");
-    });
-};
-
-handleDelete = (event) => {
-  event.preventDefault();
-  axios({
-    url: "/api/tweets/:id",
-    method: "delete",
-    data: this.state.posts,
-  })
-    .then(() => {
-      console.log("Data Deleted!");
-    })
-<<<<<<< HEAD
-    .catch(() => {
-      console.log("Server Error!");
-    });
-};
-=======
       .then(() => {
         console.log("Data Deleted!");
       })
@@ -70,30 +29,62 @@ handleDelete = (event) => {
         console.log(err);
       });
   };
->>>>>>> f5663a4fdd95a10c972af985eb959af1ee29986c
 
-render();
-{
-  return (
-    <div className="savedList">
-      {this.state.posts.map((post) => (
-        <div className="savedTweet">
-          <img className="pic" src={post.pic} alt=""></img>
-          <h2 className="name">{post.name}</h2>
-          <h6 className="userName">@{post.username}</h6>
-          <p className="following">{post.following} Following</p>
-          <p className="followers">{post.followers} Followers</p>
-          <p className="userTweet">{post.userTweet} Tweets</p>
-          <p className="tweet">{post.tweet}</p>
-          <p className="date">{post.date}</p>
-          <p className="likes">{post.likes} Likes</p>
-          <p className="retweets">{post.retweets} Retweets</p>
-          <p className="replies">{post.replies} Replies</p>
-          <DeleteButton handleDelete={this.handleDelete} />
-        </div>
-      ))}
-    </div>
-  );
+  getTweets = () => {
+    axios
+      .get("/api/tweets")
+      .then((response) => {
+        const data = response.data;
+        console.log("Data Received!");
+        this.setState({ posts: data });
+        console.log(this.state.posts);
+      })
+      .catch(() => {
+        console.log("Error with Database!");
+      });
+  };
+
+  handleDelete = (event) => {
+    event.preventDefault();
+    axios({
+      url: "/api/tweets/:id",
+      method: "delete",
+      data: this.state.posts,
+    })
+      .then(() => {
+        console.log("Data Deleted!");
+      })
+      .then(() => {
+        console.log("Data Deleted!");
+      })
+      .catch((err) => {
+        console.log("Server Error!");
+        console.log(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="savedList">
+        {this.state.posts.map((post) => (
+          <div className="savedTweet">
+            <img className="pic" src={post.pic} alt=""></img>
+            <h2 className="name">{post.name}</h2>
+            <h6 className="userName">@{post.username}</h6>
+            <p className="following">{post.following} Following</p>
+            <p className="followers">{post.followers} Followers</p>
+            <p className="userTweet">{post.userTweet} Tweets</p>
+            <p className="tweet">{post.tweet}</p>
+            <p className="date">{post.date}</p>
+            <p className="likes">{post.likes} Likes</p>
+            <p className="retweets">{post.retweets} Retweets</p>
+            <p className="replies">{post.replies} Replies</p>
+            <DeleteButton handleDelete={this.handleDelete} />
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default SavedTweets;
